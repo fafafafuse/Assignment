c98fb0767bfef8ffff06ca91286db9e7e27d2893
fafafafuse <rhazic521@gmail.com>
2018-02-15 12:09:09 +0700

    simply show mobileNo. and their calling cost

--- read-json/src/App.js
+++ read-json/src/App.js
@@ -1,24 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import data from './CallingPhoneCost.json';
+import data from './PhoneCallingCost.json';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <ul>
         {
           data.map(function(phone){
-            return <li>{phone.mobileNo} - {phone.cost}</li>;
+            return <li>Mobile No.: {phone.mobileNo} - {phone.cost.toFixed(2)} Baht</li>;
           })
         }
         </ul>
-      </div>
+        </div>
     );
   }
 }
