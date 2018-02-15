1d13a078a9a8b958f9bb2e6a93a5538475b38850
fafafafuse <rhazic521@gmail.com>
2018-02-15 11:11:32 +0700

    remove json from root

--- read-json/src/App.js
+++ read-json/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import data from './CallingPhoneCost.json';
 import './App.css';
 
 class App extends Component {
@@ -10,9 +11,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul>
+        {
+          data.map(function(phone){
+            return <li>{phone.mobileNo} - {phone.cost}</li>;
+          })
+        }
+        </ul>
       </div>
     );
   }
