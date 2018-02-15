8704bf9a9803c64f1cdd7b2e3d0c0f2039a2c4f8
fafafafuse <rhazic521@gmail.com>
2018-02-15 13:17:02 +0700

    save update
    update simply display total cost

--- read-json/src/App.js
+++ read-json/src/App.js
@@ -7,13 +7,19 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <ul>
+        <header className="App-header"><h1>Customer Toal Cost</h1></header>
+        
+        <table>
+        <tr><th>Mobile No.</th><th>Total Cost</th></tr>
         {
           data.map(function(phone){
-            return <li>Mobile No.: {phone.mobileNo} - {phone.cost.toFixed(2)} Baht</li>;
+            return <tr className="list">
+            <td className="Table-mobileNo">{phone.mobileNo}</td>  
+            <td className="Table-cost">{phone.cost.toFixed(2)}</td>
+            </tr>
           })
         }
-        </ul>
+        </table>
         </div>
     );
   }
